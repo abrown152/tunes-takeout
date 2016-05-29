a39730a6ad66dc395c16b75d6a6ab46efaaff04e
Alysia Brown <alysiacbrown@gmail.com>
2016-05-29 15:57:14 -0700

    Refactored suggestions controller methods.

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,11 +5,11 @@ class SuggestionsController < ApplicationController
   #   @results = TunesTakeoutWrapper.search(params[:keyword])
   # end
 
-  def index
-    @results = get_suggestions
-  end
+  # def index
+  #   @results = get_suggestions
+  # end
 
-  def get_suggestions
+  def index
     results = TunesTakeoutWrapper.top_twenty
     @results = results["suggestions"]
     # results["suggestions"].map do |suggestion_id|
@@ -23,16 +23,16 @@ class SuggestionsController < ApplicationController
     # end
   end
 
-  def get_music(suggestion)
-    case suggestion["music_type"]
-    when "artist"
-      RSpotify::Artist.find(suggestion["music_id"])
-    when "track"
-      RSpotify::Track.find(suggestion["music_id"])
-    when "album"
-      RSpotify::Album.find(suggestion["music_id"])
-    end
-  end
+  # def get_music(suggestion)
+  #   case suggestion["music_type"]
+  #   when "artist"
+  #     RSpotify::Artist.find(suggestion["music_id"])
+  #   when "track"
+  #     RSpotify::Track.find(suggestion["music_id"])
+  #   when "album"
+  #     RSpotify::Album.find(suggestion["music_id"])
+  #   end
+  # end
 
   # def get_food(suggestion)
   #   "food"

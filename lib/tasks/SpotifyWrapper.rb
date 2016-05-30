11e23759854b1182e4a9fea19b656a309400e862
Alysia Brown <alysiacbrown@gmail.com>
2016-05-30 15:29:59 -0700

    Top twenty now returns names of artists, albums, and tracks.

--- lib/tasks/SpotifyWrapper.rb
+++ lib/tasks/SpotifyWrapper.rb
@@ -1,19 +1,26 @@
 require 'httparty'
+require 'rspotify'
 
 class SpotifyWrapper
   BASE_URL = "https://tunes-takeout-api.herokuapp.com/"
 
-  def self.find_album(music_id)
-    album = RSpotify::Album.find(music_id)
+  def self.hello
+    artists = RSpotify::Artist.search('Arctic Monkeys')
+    arctic_monkeys = artists.first
+    arctic_monkeys.popularity
   end
 
-  def self.find_artist(music_id)
-    album = RSpotify::Artist.find(music_id)
-    # HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
-  end
-
-  def self.find_track(music_id)
-    album = RSpotify::Track.find(music_id)
-    # HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
-  end
+  # def self.find_album(music_id)
+  #   RSpotify::Album.find(music_id)
+  # end
+  #
+  # def self.find_artist(music_id)
+  #   RSpotify::Artist.find(music_id)
+  #   # HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
+  # end
+  #
+  # def self.find_track(music_id)
+  #   RSpotify::Track.find(music_id)
+  #   # HTTParty.get(BASE_URL + "v1/suggestions/search?query=" + keyword).parsed_response
+  # end
 end
